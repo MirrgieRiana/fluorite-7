da3413bc7f76831f7fab1fd0c9021a13954e4069
Mirrgie Riana <tacticsrealize2@gmail.com>
2020-03-30 14:50:49 +0900

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+if [ -d fluorite-7 ]
+then
+  (
+    cd fluorite-7
+    git checkout -f
+    git clean -fd
+    git pull
+    ./compile.bash
+  )
+else
+  GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=no' git clone --depth=1 https://github.com/MirrgieRiana/fluorite-7.git
+fi
+echo -e '#!/usr/bin/env bash\nexec "$(dirname "$0")"/fluorite-7/fl7 "$@"' > fl7
+chmod +x fl7
+(
+  cd fluorite-7
+  npm install
+)
