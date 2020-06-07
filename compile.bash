d21b1421b27496fa3dec5856abecce5405a08d35
Mirrgie Riana <tacticsrealize2@gmail.com>
2020-06-07 20:33:01 +0900

    Update compile.bash

--- compile.bash
+++ compile.bash
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-node_modules/pegjs/bin/pegjs --cache --allowed-start-rules Root fluorite-7.pegjs
+node_modules/pegjs/bin/pegjs --cache --allowed-start-rules Root,RootEmbeddedFluorite fluorite-7.pegjs
