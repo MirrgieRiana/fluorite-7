6eeb58cc22200aa0f507ecec0a6af79e20357223
Mirrgie Riana <tacticsrealize2@gmail.com>
2020-11-18 14:05:01 +0900

    Update install.bash

--- install.bash
+++ install.bash
@@ -11,14 +11,18 @@ then
 else
   GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=no' git clone --depth=1 https://github.com/MirrgieRiana/fluorite-7.git
 fi
+
 echo -e '#!/usr/bin/env bash\nexec "$(dirname "$0")"/fluorite-7/fl7 "$@"' > fl7
 echo -e '#!/usr/bin/env bash\nexec "$(dirname "$0")"/fluorite-7/fluorite7 "$@"' > fluorite7
 chmod +x fl7
 chmod +x fluorite7
 chmod +x fluorite-7/fl7
 chmod +x fluorite-7/fluorite7
+
 (
   cd fluorite-7
+  mkdir -p node_modules/heapdump
+  mkdir -p node_modules/pegjs
   npm install
   ./compile.bash
 )
